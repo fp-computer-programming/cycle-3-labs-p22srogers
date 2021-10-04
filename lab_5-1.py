3f513747af45153b34674bcd5939ce9f4a84287e
P22srogers <p22srogers@fairfieldprep.org>
2021-10-04 09:56:52 -0400

    Submit Cycle 3 Day 5 Lab

---
+++ lab_5-1.py
@@ -0,0 +1,6 @@
+# Author: SMR (AMDG) 10/04/21
+# int(a) Will be a Name Error. (correct)
+# int('a') Will be a Value Error. (correct)
+# 'a' +2 Will be a Type Error. (correct)
+# import date Will be a module not found error. (correct)
+# print("I'm a happy camper!) will be syntax error. (correct)
